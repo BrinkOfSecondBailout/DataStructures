654ea2d23e7b9d0155fa86e5da5cde1703d4c43c
Danny Nguyen <nguyenduongpa@gmail.com>
2023-06-19 12:02:05 -0700

    prefix

--- leetcode.js
+++ leetcode.js
@@ -37,3 +37,18 @@ var romanToInt = function(s) {
         }
     }
 };
+
+
+var longestCommonPrefix = function(strs) {
+    var prefix = strs[0]
+    for (let i = 1; i < strs.length; i++) {
+        while(strs[i].indexOf(prefix) !== 0) {
+            prefix = prefix.substring(0, prefix.length - 1)
+        }
+    }
+    return prefix;
+};
+
+strs = ["flower","flow","flight"]
+
+console.log(longestCommonPrefix(strs))
