44d82c5a174ac6ac54d7535e03cc5d724f0c524a
Danny Nguyen <nguyenduongpa@gmail.com>
2023-06-19 11:23:21 -0700

    roman

--- leetcode.js
+++ leetcode.js
@@ -15,3 +15,25 @@ var twoSum = function(nums, target) {
 
 nums = [3,8,6,4,5,3], target = 6
 console.log(twoSum(nums, target));
+
+var romanToInt = function(s) {
+    let romanPairs = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+    }
+    let total = 0;
+    for (let i = 0; i < s.length; i++) {
+        let curr = s[i];
+        let next = s[i + 1];
+        if (romanPairs[curr] < romanPairs[next]) {
+            total -= romanPairs[curr]
+        } else {
+            total += romanPairs[curr]
+        }
+    }
+};
