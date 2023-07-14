0c8e1ea5cb51295e8e9579e6b82729b1f25ed1ca
Danny Nguyen <nguyenduongpa@gmail.com>
2023-07-14 16:16:05 -0700

    valid parentheses

--- leetcode.js
+++ leetcode.js
@@ -160,8 +160,28 @@ strs = ["flower","flow","flight"]
 console.log(longestCommonPrefix(strs))
 
 var isValid = function(s) {
-    
-};
+    const pairs = {
+        "(": ")",
+        "[": "]",
+        "{": "}"
+    }
+    if (s.length % 2 != 0) {
+        return false
+    }
+    if (s[0] == ")" || s[0] == "}" || s[0] == "]") {
+        return false
+    }
+    var stack = []
+
+    for (let i = 0; i < s.length; i ++) {
+        if (s[i] === "(" || s[i] === "{" || s[i] === "[") {
+            stack.push(s[i])
+        } else if (pairs[stack.pop()] != s[i]) {
+            return false
+        }
+    }
+    return stack.length === 0
+}
 
 s = "()[]{}"
 
