8f3710d02661ee9568afe7d646bfe363c188faa3
Danny Nguyen <nguyenduongpa@gmail.com>
2023-07-16 12:02:46 -0700

    remove dupe

--- leetcode.js
+++ leetcode.js
@@ -313,3 +313,33 @@ var longestCommonPrefix = function(strs) {
 strs = ["flower","flow","flight"]
 
 console.log(longestCommonPrefix(strs))
+
+
+
+
+var removeDuplicates = function(nums) {
+    var newArr = []
+    for (let i = 0; i < nums.length; i ++) {
+        while (newArr.indexOf(nums[i]) === -1) {
+            newArr.push(nums[i])
+        }
+    }
+    return newArr
+};
+
+var removeDuplicates = function(nums) {
+    let i = 0;
+    for (let j = 1; j < nums.length; j++) {
+        if (nums[i] !== nums[j]) {
+            i++;
+            nums[i] = nums[j];
+        }
+    }
+    return nums;
+};
+
+nums = [0,0,1,1,1,2,2,3,3,4]
+
+
+
+console.log(removeDuplicates(nums))
