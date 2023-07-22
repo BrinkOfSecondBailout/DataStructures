da5df7a784f2adcf3de51be72d5f471ec6f88c8d
Danny Nguyen <nguyenduongpa@gmail.com>
2023-07-21 18:31:13 -0700

    list node

--- leetcode.js
+++ leetcode.js
@@ -463,3 +463,37 @@ const myArray = text.split(" ")
 
 console.log(myArray)
 
+
+class ListNode {
+    constructor(val, next = null) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+var addTwoNumbers = function(l1, l2) {
+    let carry = 0;
+    let result = new ListNode(1);
+    let resultPointer = result;
+
+    while(l1 || l2 || carry) {
+        let l1Val = l1 ? l1.val : 0;
+        let l2Val = l2 ? l2.val : 0;
+
+        let nextDigit = (l1Val + l2Val + carry) % 10;
+        result.next = new ListNode(nextDigit);
+        result = result.next;
+
+        carry = Math.floor((l1Val + l2Val + carry) / 10);
+
+        l1 = l1 ? l1.next : null;
+        l2 = l2 ? l2.next : null;
+    }
+
+    return resultPointer.next;
+};
+
+let l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
+let l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+
+console.log(addTwoNumbers(l1, l2))
