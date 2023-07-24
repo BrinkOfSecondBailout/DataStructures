408edc06480514c614fc176c6d6e95199a506123
Danny Nguyen <nguyenduongpa@gmail.com>
2023-07-23 19:31:25 -0700

    push

--- leetcode.js
+++ leetcode.js
@@ -468,7 +468,45 @@ var addTwoNumbers = function(l1, l2) {
     return resultPointer.next;
 };
 
-let l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
-let l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+var l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
+var l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+
+console.log(addTwoNumbers(l1, l2))
+
+
+
+
+
+
+
+class ListNode {
+    constructor(val, next = null) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+
+var l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
+var l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+
+var addTwoNumbers = function(l1, l2) {
+    let carry = 0;
+    let result = new ListNode(1);
+    let resultPointer = result;
+    while (l1 || l2 || carry) {
+        let l1Val = l1 ? l1.val : 0;
+        let l2Val = l2 ? l2.val : 0;
+        let nextDigit = ((l1Val + l2Val + carry) % 10);
+        result.next = new ListNode(nextDigit);
+        result = result.next;
+        carry = Math.floor((l1Val + l2Val + carry) / 10);
+
+        l1 = l1 ? l1.next : 0;
+        l2 = l2 ? l2.next : 0;
+    }
+    return resultPointer.next;
+};
+
 
 console.log(addTwoNumbers(l1, l2))
