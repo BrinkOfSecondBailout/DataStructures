418f9b719c57dd177e417c4a302e01220be25479
Danny Nguyen <nguyenduongpa@gmail.com>
2023-08-04 16:53:58 -0700

    static method

--- script.js
+++ script.js
@@ -8,6 +8,15 @@ class Car {
     carStats() {
         return `This car has ${this.doors} doors, a ${this.engine} engine, and a beautiful ${this.color} color`;
     }
+
+    static totalDoors(car1, car2) {
+        const doors1 = car1.doors;
+        const doors2 = car2.doors;
+        return doors1 + doors2;
+    }
 }
 
 const cx5 = new Car(4, 'V6', 'Grey');
+
+console.log(cx5);
+console.log(cx5.carStats());
