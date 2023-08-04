afe064b10425cdcc88307030a1b8f981c91750d2
Danny Nguyen <nguyenduongpa@gmail.com>
2023-08-04 14:12:40 -0700

    m

--- script.js
+++ script.js
@@ -4,4 +4,10 @@ class Car {
         this.engine = engine;
         this.color = color;
     }
+
+    carStats() {
+        return `This car has ${this.doors} doors, a ${this.engine} engine, and a beautiful ${this.color} color`;
+    }
 }
+
+const cx5 = new Car(4, 'V6', 'Grey');
