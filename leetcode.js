1f441782f5807085a9ed3b543c180e8fa5c5c431
Danny Nguyen <nguyenduongpa@gmail.com>
2023-08-13 12:55:19 -0700

    solved

--- leetcode.js
+++ leetcode.js
@@ -599,14 +599,17 @@ console.log(commonItems(array1, array2));
 // O(a + b)
 
 
-function reverse(string) {
-    var newString = []
-    console.log(string)
-    for (let i = string.length - 1; i = 0; i--) {
-        newString.push(string[i]);
-        console.log(string[i])
+function reverse(str) {
+    if (!str || str.length < 2 || typeof str !== "string") {
+        return 'wrong data type';
     }
-    return newString;
+    const newArr = [];
+    const totalItems = str.length - 1;
+    for (let i = totalItems; i >= 0; i--) {
+        newArr.push(str[i])
+    }
+    console.log(newArr)
+    return newArr.join('');
 }
 
 let str = 'Hi my name is Danny';
