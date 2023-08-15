5c109f83f9539fb78c41c6a6fc048cb156d9d0c5
Danny Nguyen <nguyenduongpa@gmail.com>
2023-08-15 14:28:47 -0700

    hashtables

--- leetcode.js
+++ leetcode.js
@@ -739,3 +739,47 @@ function moveZeroes(nums) {
 
 var nums = [7, 0, 3, 5, 2, 1];
 console.log(moveZeroes(nums));
+
+
+
+
+class HashTable {
+    constructor(size) {
+        this.data = new Array(size);
+    }
+
+    _hash(key) {
+        let hash = 0;
+        for(let i = 0; i < key.length; i++) {
+            hash = (hash + key.charCodeAt(i) * i) % this.data.length
+        }
+        return hash;
+    }
+
+    set(key, value) {
+        let address = this._hash(key);
+        if(!this.data[address]) {
+            this.data[address] = [];
+        }
+        this.data[address].push([key, value]);
+        return this.data;
+    }
+
+    get(key) {
+        let address = this._hash(key);
+        const currentBucket = this.data[address];
+        if(currentBucket) {
+            for(let i = 0; i < currentBucket.length; i++) {
+                if (currentBucket[i][0] === key) {
+                    return currentBucket[i][1];
+                }
+            }
+        } else {
+            return undefined;
+        }
+    }
+}
+
+const myHashTable = new HashTable(50);
+console.log(myHashTable.set('grapes', 1000));
+console.log(myHashTable.get('grapes'));
