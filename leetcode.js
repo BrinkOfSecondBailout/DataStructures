0e7a06fe9306bae8424088b7c13ff51b84f44746
Danny Nguyen <nguyenduongpa@gmail.com>
2023-08-16 11:52:15 -0700

    insert done

--- leetcode.js
+++ leetcode.js
@@ -852,6 +852,38 @@ class LinkedList {
         this.length++;
         return this;
     }
+    printList() {
+        const array = [];
+        let currentNode = this.head;
+        while (currentNode !== null) {
+            array.push(currentNode.value)
+            currentNode = currentNode.next
+        }
+        return array;
+    }
+    insert(index, value) {
+        if (index >= this.length) {
+            return this.append(value);
+        }
+        if (index === 0) {
+            return this.prepend(value);
+        }
+        const newNode = new Node(value);
+        const leader = this.traverseToIndex(index - 1);
+        newNode.next = leader.next;
+        leader.next = newNode;
+        this.length++;
+        return this;
+    }
+    traverseToIndex(index) {
+        let counter = 0;
+        let currentNode = this.head;
+        while (counter !== index) {
+            currentNode = currentNode.next;
+            counter++;
+        }
+        return currentNode;
+    }
 }
 
 
@@ -860,4 +892,5 @@ const myLinkedList = new LinkedList(10);
 myLinkedList.append(5);
 myLinkedList.append(16);
 myLinkedList.prepend(1);
-console.log(myLinkedList);
+myLinkedList.insert(2, 99);
+console.log(myLinkedList.printList());
