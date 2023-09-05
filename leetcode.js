1caa9678e69900e37951c77ec50fccdd7a22d5af
Danny Nguyen <nguyenduongpa@gmail.com>
2023-09-05 09:42:38 -0700

    queue

--- leetcode.js
+++ leetcode.js
@@ -1109,3 +1109,31 @@ class Stack {
         return this;
     }
 }
+
+class Queue {
+    constructor() {
+        this.first = null;
+        this.last = null;
+        this.length = 0;
+    }
+    peek() {
+        return this.first;
+    }
+    enqueue(value) {
+        const newNode = new Node(value);
+        if(this.length === 0) {
+            this.first = newNode;
+            this.last = newNode;
+            this.length++;
+            return this;
+        }
+        this.last.next = newNode;
+        this.last = newNode;
+        this.length++;
+        return this;
+    }
+    dequeue() {
+
+    }
+}
+
