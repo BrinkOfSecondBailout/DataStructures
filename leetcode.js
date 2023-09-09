5a1f5819fd8be076b81da12f86e22db72ef54d10
Danny Nguyen <nguyenduongpa@gmail.com>
2023-09-09 08:14:07 -0700

    add connections

--- leetcode.js
+++ leetcode.js
@@ -1325,8 +1325,19 @@ class Graph {
         this.numberOfNodes++;
     }
     addEdge(node1, node2) {
-
+        this.adjacentList[node1].push(node2);
+        this.adjacentList[node2].push(node1);
     }
     showConnections() {
+        const allNodes = Object.keys(this.adjacentList);
+        for (let node of allNodes) {
+            let nodeConnections = this.adjacentList[node];
+            let connections = "";
+            let vertex;
+            for (vertex of nodeConnections) {
+                connections += vertex + " ";
+            }
+            console.log(node + "-->" + connections);
+        }
     }
 }
