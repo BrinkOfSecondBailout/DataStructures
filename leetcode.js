b651de2e8e1fc79b34b9ed1e978d5462bf5dc0c4
Danny Nguyen <nguyenduongpa@gmail.com>
2023-09-08 18:20:37 -0700

    graphs

--- leetcode.js
+++ leetcode.js
@@ -1296,3 +1296,37 @@ const graph = [[0, 2], [2, 3], [2, 1], [1, 3]];
 // Adjacent List
 
 const graph = [[2], [2, 3], [0, 1, 3], [1, 2]];
+
+// Adjacent matrix
+
+const graph = [
+    [0, 0, 1, 0],
+    [0, 0, 1, 1],
+    [1, 1, 0, 1],
+    [0, 1, 1, 0]
+]
+
+// Or
+
+const graph = {
+    0: [0, 0, 1, 0],
+    1: [0, 0, 1, 1],
+    2: [1, 1, 0, 1],
+    3: [0, 1, 1, 0]
+}
+
+class Graph {
+    constructor() {
+        this.numberOfNodes = 0;
+        this.adjacentList = {};
+    }
+    addVertex(node) {
+
+    }
+    addEdge(node1, node2) {
+
+    }
+    showConnections() {
+        
+    }
+}
