44b7346cd644560b005349e68da5267ba9a646fb
Danny Nguyen <nguyenduongpa@gmail.com>
2023-09-13 09:13:07 -0700

    d

--- leetcode.js
+++ leetcode.js
@@ -1507,11 +1507,11 @@ console.log(myQuickSort(numbers4));
 
 
 // left and right are sorted!
-function merge(left, right) {
+function myMerge(left, right) {
     const output = [];
     let leftIndex = 0;
     let rightIndex = 0;
-    while(leftIndex < left.length || rightIndex < right.length) {
+    while(leftIndex < left.length && rightIndex < right.length) {
         const leftEl = left[leftIndex];
         const rightEl = right[rightIndex];
         if (leftEl < rightEl) {
@@ -1525,15 +1525,22 @@ function merge(left, right) {
     return [...output, ...left.slice(leftIndex), ...right.slice(rightIndex)];
 }
 
+
+
 function myMergeSort(array) {
-    if(array.length = 1) {
+    if(array.length <= 1) {
         return array;
     }
     const middle = Math.floor(array.length / 2);
     const left = array.slice(0, middle);
     const right = array.slice(middle);
-    return merge(
+    return myMerge(
         myMergeSort(left),
         myMergeSort(right)
     );
 }
+
+
+let numbers5 = [1, 4, 2, 23, 44, 8, 599, 911, 33, 0];
+
+console.log(myMergeSort(numbers5));
