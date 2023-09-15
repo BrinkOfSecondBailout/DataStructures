98a61a1f788fddc1985ed3e9ab387efebf78f7ff
Danny Nguyen <nguyenduongpa@gmail.com>
2023-09-14 18:58:17 -0700

    d

--- leetcode.js
+++ leetcode.js
@@ -1565,3 +1565,126 @@ function myInsertionSort(array) {
 
 let numbers6 = [1, 4, 2, 23, 44, 8, 599, 911, 33, 0];
 console.log(myInsertionSort(numbers6));
+
+
+
+
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+
+class BinarySearchTree {
+    constructor() {
+        this.root = null;
+    }
+    insert(value) {
+        const newNode = new Node(value);
+        if(this.root === null) {
+            this.root = newNode;
+        } else {
+            let currentNode = this.root;
+            while(true) {
+                if(value < currentNode.value) {
+                    if(!currentNode.left) {
+                        currentNode.left = newNode;
+                        return this;
+                    } 
+                    currentNode = currentNode.left;
+                } else {
+                    if(!currentNode.right) {
+                        currentNode.right = newNode;
+                        return this;
+                    }
+                    currentNode = currentNode.right;
+                }
+            }
+        }
+    }
+    lookup(value) {
+        if(this.root === null) {
+            return false;
+        }
+        let currentNode = this.root
+        while(currentNode) {
+            if(value > currentNode.value) {
+                currentNode = currentNode.right;
+            } else if (value < currentNode.value) {
+                currentNode = currentNode.left;
+            } else if (value === currentNode.value) {
+                return currentNode;
+            }
+        }
+        return false;
+    }
+    remove(value) {
+        if(this.root === null) {
+            return false;
+        }
+        let currentNode = this.root;
+        let parentNode = null;
+        while(currentNode) {
+            if(value > currentNode.value) {
+                parentNode = currentNode;
+                currentNode = currentNode.right;
+            } else if (value < currentNode.value) {
+                parentNode = currentNode;
+                currentNode = currentNode.left;
+            } else if (value === currentNode.value) {
+                parentNode = currentNode;
+                if(currentNode.right === null) {
+                    if(parentNode === null) {
+                        this.root = currentNode.left;
+                    } else {
+                        if(currentNode.value < parentNode.value) {
+                            parentNode.left = currentNode.left;
+                        } else if (currentNode.value > parentNode.value) {
+                            parentNode.right = currentNode.left;
+                        }
+                    }
+                } else if(currentNode.right.left === null) {
+                    if(parentNode === null) {
+                        this.root = currentNode.left;
+                    } else {
+                        parentNode.right = currentNode.right;
+                    }
+                } else {
+                    let leftmost = currentNode.right.left;
+                    let leftmostParent = currentNode.right;
+                    while(leftmost.left !== null) {
+                        leftmostParent = leftmost;
+                        leftmost = leftmost.left;
+                    }
+                    leftmostParent.left = leftmost.right;
+                    leftmost.left = currentNode.left;
+                    leftmost.right = currentNode.right;
+
+                    if(parentNode === null) {
+                        this.root = leftmost;
+                    } else {
+                        if(currentNode.value < parentNode.value) {
+                            parentNode.left = leftmost;
+                        } else if(currentNode.value > parentNode.value) {
+                            parentNode.right = leftmost;
+                        }
+                    }
+                }
+            return true;
+            }
+        }
+    }
+}
+
+const tree = new BinarySearchTree();
+tree.insert(9);
+tree.insert(4);
+tree.insert(6);
+tree.insert(20);
+tree.insert(170);
+tree.insert(15);
+tree.insert(1);
+console.log(tree.lookup(1));
