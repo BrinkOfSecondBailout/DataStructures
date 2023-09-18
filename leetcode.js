ad7923807f1527c5da5420b46c4b1b9317e62c51
Danny Nguyen <nguyenduongpa@gmail.com>
2023-09-18 12:23:37 -0700

    d

--- leetcode.js
+++ leetcode.js
@@ -1813,8 +1813,16 @@ function fibonacci() {
     }
 }
 
-
-
 const dynamicFib = fibonacci();
-
 console.log(dynamicFib(100));
+
+function fibonacci2(n) {
+    let answer = [0, 1];
+    for (let i = 2; i <= n; i++) {
+        answer.push(answer[i-2] + answer[i-1]);
+    }
+    return answer.pop();
+}
+
+
+console.log(fibonacci2(8));
