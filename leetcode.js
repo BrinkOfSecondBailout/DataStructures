ee46618a45d25a4e31759f0393199df5205f5755
Danny Nguyen <nguyenduongpa@gmail.com>
2023-09-23 18:55:10 -0700

    d

--- leetcode.js
+++ leetcode.js
@@ -1,18 +1,3 @@
-var twoSum = function(nums, target) {
-    const memoryObj = {};
-    const output = [];
-    for (let i = 0; i < nums.length; i ++) {
-        const difference = target - nums[i]
-        if (difference in memoryObj) {
-            output.push(i);
-            output.push(memoryObj[difference])
-            return output;
-        } else {
-            memoryObj[nums[i]] = i;
-        }
-    }
-};
-
 nums = [3,8,6,4,5,3], target = 6
 console.log(twoSum(nums, target));
 
