c4e11fe8f5f67555043d36f75c9cdabef083a283
Danny Nguyen <nguyenduongpa@gmail.com>
2023-11-24 15:34:26 -0800

    d

--- leetcode2.js
+++ leetcode2.js
@@ -1,11 +1,46 @@
-const myObject = {
-    key1: new Set(),
-    key2: new Set([1, 2, 3]),
-    key3: new Set(['apple', 'banana'])
-};
+// const myObject = {
+//     key1: new Set(),
+//     key2: new Set([1, 2, 3]),
+//     key3: new Set(['apple', 'banana'])
+// };
 
-myObject.key1.add('orange');
-myObject.key2.add(4);
-myObject.key3.add('orange');
+// myObject.key1.add('orange');
+// myObject.key2.add(4);
+// myObject.key3.add('orange');
+// myObject.key3.add('apple');
 
-console.log(myObject);
+// console.log(myObject);
+
+function analyzeUserLogs(logs) {
+
+    const userCounts = {};
+
+    logs.forEach(log => {
+        const [date, user] = log.split(', ');
+
+        if(userCounts.hasOwnProperty(date)) {
+            userCounts[date].add(user);
+        } else {
+            userCounts[date] = new Set([user]);
+        }
+    })
+
+
+    for (const date in userCounts) {
+        if(userCounts.hasOwnProperty(date)) {
+            const uniqueUserCount = userCounts[date].size;
+            console.log(`${date}: ${uniqueUserCount}`);
+        }
+    }
+
+}
+
+const logs = [
+    "2023-11-01, User1",
+    "2023-11-01, User2",
+    "2023-11-01, User1",
+    "2023-11-02, User2",
+    "2023-11-02, User3"
+];
+
+analyzeUserLogs(logs);
