09dd73024e9c5bac79299496ed5c0537e06229af
Danny Nguyen <nguyenduongpa@gmail.com>
2023-11-24 14:46:04 -0800

    d

--- leetcode2.js
+++ leetcode2.js
@@ -1,3 +1,11 @@
-var merge = function(nums1, m, nums2, n) {
-    
+const myObject = {
+    key1: new Set(),
+    key2: new Set([1, 2, 3]),
+    key3: new Set(['apple', 'banana'])
 };
+
+myObject.key1.add('orange');
+myObject.key2.add(4);
+myObject.key3.add('orange');
+
+console.log(myObject);
