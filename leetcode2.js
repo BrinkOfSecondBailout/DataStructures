91d313e21bb0e4994cff604fa7dcf11802f2aef4
Danny Nguyen <nguyenduongpa@gmail.com>
2023-11-27 18:26:56 -0800

    d

--- leetcode2.js
+++ leetcode2.js
@@ -244,8 +244,11 @@
 
 
 var merge = function(nums1, m, nums2, n) {
-    const output = [];
-    
+    for (let i = m, j = 0; j < n; i++, j++) {\
+        nums1[i] = nums2[j];
+    }
+    nums1.sort((a, b) => a - b);
+    return nums1;
 };
 
 
