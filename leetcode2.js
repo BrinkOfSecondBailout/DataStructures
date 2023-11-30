ddd00004db1bfce8605767e2c79e98f4f80b6339
Danny Nguyen <nguyenduongpa@gmail.com>
2023-11-30 08:49:09 -0800

    d

--- leetcode2.js
+++ leetcode2.js
@@ -349,20 +349,34 @@
 
 // }
 
+// leetcode solution
+// const removeDuplicates2 = (nums) => {
+//     let j = 2;
+
+//     for (let i = 2; i < nums.length; i++) {
+//         if (nums[i] !== nums[j - 2]) {
+//             nums[j] = nums[i];
+//             j++;
+//         }
+//     }
+
+//     return nums;
+// };
 
 const removeDuplicates2 = (nums) => {
-    let j = 2;
+    let j = 1;
 
-    for (let i = 2; i < nums.length; i++) {
-        if (nums[i] !== nums[j - 2]) {
-        nums[j++] = nums[i];
+    for(let i = 1; i < nums.length; i++) {
+        if(nums[i] !== nums[j-1]) {
+            nums[j] = nums[i];
+            j++;
         }
     }
-
     return nums;
 };
 
 
-nums1 = [1,1,1,2,2,1,3,4];
+
+nums1 = [0,0,1,1,1,1,2,3,3]
 
 console.log(removeDuplicates2(nums1));
