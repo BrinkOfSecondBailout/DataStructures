02d8caba6583e9d4e489244acb4dccd1a631ac56
Danny Nguyen <nguyenduongpa@gmail.com>
2024-03-31 08:59:31 -0700

    java

---
+++ java/Cat.java
@@ -0,0 +1,7 @@
+package java;
+
+public class Cat implements Animal{
+    public void makeSound() {
+        System.out.println("Meow");
+    }
+}
---
+++ java/Dog.java
@@ -0,0 +1,7 @@
+package java;
+
+public class Dog implements Animal {
+    public void makeSound() {
+        System.out.println("Woof");
+    }
+}
