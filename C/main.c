854487ac61c20f66db6d9dacbeb4bebe9f2fe381
Danny Nguyen <nguyenduongpa@gmail.com>
2024-05-02 12:49:56 -0700

    d

---
+++ C/main.c
@@ -0,0 +1,17 @@
+// int main() {
+//     hello();
+//     return 0;
+// }
+#include <stdio.h>
+
+int main() {
+    unsigned int x = 9;
+
+    unsigned int y = 24;
+
+    unsigned int r;
+
+    r = ~x;
+
+    printf("r: %d\n", r);
+}
