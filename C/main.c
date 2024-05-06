86d3bc1fd1d90e468ab1d211a5b350e39505355d
Danny Nguyen <nguyenduongpa@gmail.com>
2024-05-06 08:49:46 -0700

    d

--- C/main.c
+++ C/main.c
@@ -1,17 +1,34 @@
-// int main() {
-//     hello();
-//     return 0;
-// }
 #include <stdio.h>
 
+#define USER        0b1
+#define ADMIN       0b10
+#define DEV         0b100
+#define OWNER       0b1000
+
 int main() {
-    unsigned int x = 9;
+    unsigned int flags = USER;
 
-    unsigned int y = 24;
+    void removeAccess(unsigned int access) {
+        flags = flags & ~access;
+    }
 
-    unsigned int r;
+    void increaseAccess(unsigned int access) {
+        flags = flags | access;
+    }
 
-    r = ~x;
+    void showAllAccess() {
+        if (flags & OWNER) {
+            printf("Owner access available.\n");
+        } else if (flags & DEV) {
+            printf("Dev access available.\n");
+        } else if (flags & ADMIN) {
+            printf("Admin access available.\n");
+        } else if (flags & USER) {
+            printf("User access available.\n");
+        }
+    }
 
-    printf("r: %d\n", r);
+    increaseAccess(OWNER);
+
+    showAllAccess();
 }
