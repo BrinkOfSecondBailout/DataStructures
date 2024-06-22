4ddd13b3993b1441ff5af5ef07c41a3311b3aa2e
Danny Nguyen <nguyenduongpa@gmail.com>
2024-06-22 08:08:07 -0700

    d

--- newleetcode4.js
+++ newleetcode4.js
@@ -2,13 +2,13 @@ var twoSum = function(nums, target) {
     var memoryObj = {};
     var results = [];
     for (let i = 0; i < nums.length; i++) {
-        var difference = target - nums[i]
+        var difference = target - nums[i];
         if (difference in memoryObj) {
             results.push(i);
             results.push(memoryObj[difference]);
             return results;
         } else {
-            memoryObj[nums[i]] = nums[i]
+            memoryObj[nums[i]] = i;
         }
     }
     return results;
