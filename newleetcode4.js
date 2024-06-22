dc1b5373fcb14df8d6cc0c9d01734c9f80f40003
Danny Nguyen <nguyenduongpa@gmail.com>
2024-06-22 08:14:30 -0700

    d

--- newleetcode4.js
+++ newleetcode4.js
@@ -1,19 +1,30 @@
-var twoSum = function(nums, target) {
-    var memoryObj = {};
-    var results = [];
-    for (let i = 0; i < nums.length; i++) {
-        var difference = target - nums[i];
-        if (difference in memoryObj) {
-            results.push(i);
-            results.push(memoryObj[difference]);
-            return results;
-        } else {
-            memoryObj[nums[i]] = i;
-        }
-    }
-    return results;
+// var twoSum = function(nums, target) {
+//     var memoryObj = {};
+//     var results = [];
+//     for (let i = 0; i < nums.length; i++) {
+//         var difference = target - nums[i];
+//         if (difference in memoryObj) {
+//             results.push(i);
+//             results.push(memoryObj[difference]);
+//             return results;
+//         } else {
+//             memoryObj[nums[i]] = i;
+//         }
+//     }
+//     return results;
+// };
+
+// nums = [2,7,11,15];
+// target = 9;
+// console.log(twoSum(nums, target));
+
+
+var addTwoNumbers = function(l1, l2) {
+    
 };
 
-nums = [2,7,11,15];
-target = 9;
-console.log(twoSum(nums, target));
+l1 = [2,4,3];
+
+l2 = [5,6,4];
+
+console.log(addTwoNumbers(l1, l2));
