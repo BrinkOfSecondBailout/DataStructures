c68b0b1a9cd2394bfcf26a44610010e666c1f30b
Danny Nguyen <nguyenduongpa@gmail.com>
2024-06-23 17:26:52 -0700

    d

--- newleetcode4.js
+++ newleetcode4.js
@@ -18,13 +18,30 @@
 // target = 9;
 // console.log(twoSum(nums, target));
 
+function ListNode(val, next) {
+    this.val = (val==undefined ? 0 : val)
+    this.next = (next==undefined ? null : next)
+}
 
-var addTwoNumbers = function(l1, l2) {
+function convertToList(array) {
+    var newList = ListNode();
+    console.log(newList.val);
+    console.log(newList.next);
+    // for (let i = 0; i < array.length; i++) {
+    //     newList.val = array[i];
+    //     newList.next = array[i+1];
+    // }
+    // console.log(newList);
+}
+
+// var addTwoNumbers = function(l1, l2) {
     
-};
+// };
 
 l1 = [2,4,3];
 
 l2 = [5,6,4];
 
-console.log(addTwoNumbers(l1, l2));
+convertToList(l1, l2);
+
+// console.log(addTwoNumbers(l1, l2));
