609430c960da5f2df12b466d5290a200124f290f
Danny Nguyen <nguyenduongpa@gmail.com>
2024-07-11 15:55:03 -0700

    palindromes

--- newleetcode4.js
+++ newleetcode4.js
@@ -19,57 +19,129 @@
 // console.log(twoSum(nums, target));
 
 
-function ListNode(val, next) {
-    this.val = (val===undefined ? 0 : val)
-    this.next = (next===undefined ? null : next)
+
+
+// function ListNode(val, next) {
+//     this.val = (val===undefined ? 0 : val)
+//     this.next = (next===undefined ? null : next)
+// }
+
+// var addTwoNumbers = function(l1, l2) {
+//     let dummyHead = new ListNode(0);
+//     let p = l1, q = l2, current = dummyHead;
+//     let carry = 0;
+    
+//     while (p !== null || q !== null) {
+//         let x = (p !== null) ? p.val : 0;
+//         let y = (q !== null) ? q.val : 0;
+//         let sum = carry + x + y;
+//         carry = Math.floor(sum / 10);
+//         current.next = new ListNode(sum % 10);
+//         current = current.next;
+//         if (p !== null) p = p.next;
+//         if (q !== null) q = q.next;
+//     }
+    
+//     if (carry > 0) {
+//         current.next = new ListNode(carry);
+//     }
+    
+//     return dummyHead.next;
+// };
+
+
+
+
+
+/* Given a string s, find the length of the longest 
+substring without repeating characters
+*/
+
+// var lengthOfLongestSubstring = function(s) {
+//     let n = s.length;
+//     let set = new Set();
+//     let left = 0, right = 0; maxLength = 0;
+
+//     while (right < n) {
+//         if (!set.has(s[right])) {
+//             set.add(s[right]);
+//             right++;
+//             maxLength = Math.max(maxLength, right - left);
+//         } else {
+//             set.delete(s[left]);
+//             left++;
+//         }
+//     }
+
+//     return maxLength;
+
+// };
+
+// s = "abcabcbb";
+
+// console.log(lengthOfLongestSubstring(s));
+
+
+
+var longestPalindrome = function(s) {
+    let start = 0, end = 0;
+    
+    if (s.length < 1) return "";
+
+    for (let i = 0; i < s.length; i++) {
+        let len1 = expandAroundCenter(s, i, i); // Odd length palindromes
+        let len2 = expandAroundCenter(s, i, i + 1); // Even length palindromes
+        let len = Math.max(len1, len2);
+
+        if (len > end - start) {
+            start = i - Math.floor((len - 1) / 2);
+            end = i + Math.floor(len / 2);
+        }
+    }
+    return s.substring(start, end + 1);
+};
+
+function expandAroundCenter(s, left, right) {
+    while (left >= 0 && right < s.length && s[left] == s[right]) {
+        left--;
+        right++;
+    }
+    return right - left - 1;
 }
 
 
-var addTwoNumbers = function(l1, l2) {
-    let dummyHead = new ListNode(0);
-    let p = l1, q = l2, current = dummyHead;
-    let carry = 0;
-    
-    while (p !== null || q !== null) {
-        let x = (p !== null) ? p.val : 0;
-        let y = (q !== null) ? q.val : 0;
-        let sum = carry + x + y;
-        carry = Math.floor(sum / 10);
-        current.next = new ListNode(sum % 10);
-        current = current.next;
-        if (p !== null) p = p.next;
-        if (q !== null) q = q.next;
+s = "babad";
+console.log(longestPalindrome(s));
+
+
+
+
+
+function expandAroundCenter(s, left, right) {
+    while (left <= 0 && right < s.length && s[left] == s[right]) {
+        left--;
+        right++;
     }
-    
-    if (carry > 0) {
-        current.next = new ListNode(carry);
-    }
-    
-    return dummyHead.next;
-};
+    return right - left - 1;
+}
 
-
-
-
-var addTwoNumbers = function(l1, l2) {
-    let dummyHead = new ListNode(0);
-    let p = l1, q = l2, current = dummyHead;
-    let carry = 0;
-
-    while (p !== null || q !== null) {
-        let x = (p !== null) ? p.val : 0;
-        let y = (q !== null) ? q.val : 0;
-        let sum = carry + x + y;
-        carry = Math.floor(sum / 10);
-        current.next = (sum % 10);
-        current = current.next;
-        if (p !== null) p = p.next;
-        if (q !== null) q = q.next;
+var longestPalindrome = function(s) {
+    if (s.length < 1) {
+        return "";
     }
 
-    if (carry > 0) {
-        current.next = new ListNode(carry);
+    let start = 0, end = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        let len1 = expandAroundCenter(s, i, i) // for odd palindromes
+        let len2 = expandAroundCenter(s, i, i + 1) // for even palindromes
+        let len = Math.max(len1, len2);
+
+        if (len > end - start) {
+            start = i - Math.floor((len - 1) / 2);
+            end = i + Math.floor(len / 2);
+        }
     }
 
-    return dummyHead.next;
+    return s.substring(start, end + 1);
 }
