9ff7d16b2a174560265190d1d01188a05a830a87
Danny Nguyen <nguyenduongpa@gmail.com>
2024-07-10 17:58:32 -0700

    linked list

--- newleetcode4.js
+++ newleetcode4.js
@@ -18,30 +18,58 @@
 // target = 9;
 // console.log(twoSum(nums, target));
 
+
 function ListNode(val, next) {
-    this.val = (val==undefined ? 0 : val)
-    this.next = (next==undefined ? null : next)
+    this.val = (val===undefined ? 0 : val)
+    this.next = (next===undefined ? null : next)
 }
 
-function convertToList(array) {
-    var newList = ListNode();
-    console.log(newList.val);
-    console.log(newList.next);
-    // for (let i = 0; i < array.length; i++) {
-    //     newList.val = array[i];
-    //     newList.next = array[i+1];
-    // }
-    // console.log(newList);
-}
 
-// var addTwoNumbers = function(l1, l2) {
+var addTwoNumbers = function(l1, l2) {
+    let dummyHead = new ListNode(0);
+    let p = l1, q = l2, current = dummyHead;
+    let carry = 0;
     
-// };
+    while (p !== null || q !== null) {
+        let x = (p !== null) ? p.val : 0;
+        let y = (q !== null) ? q.val : 0;
+        let sum = carry + x + y;
+        carry = Math.floor(sum / 10);
+        current.next = new ListNode(sum % 10);
+        current = current.next;
+        if (p !== null) p = p.next;
+        if (q !== null) q = q.next;
+    }
+    
+    if (carry > 0) {
+        current.next = new ListNode(carry);
+    }
+    
+    return dummyHead.next;
+};
 
-l1 = [2,4,3];
 
-l2 = [5,6,4];
 
-convertToList(l1, l2);
 
-// console.log(addTwoNumbers(l1, l2));
+var addTwoNumbers = function(l1, l2) {
+    let dummyHead = new ListNode(0);
+    let p = l1, q = l2, current = dummyHead;
+    let carry = 0;
+
+    while (p !== null || q !== null) {
+        let x = (p !== null) ? p.val : 0;
+        let y = (q !== null) ? q.val : 0;
+        let sum = carry + x + y;
+        carry = Math.floor(sum / 10);
+        current.next = (sum % 10);
+        current = current.next;
+        if (p !== null) p = p.next;
+        if (q !== null) q = q.next;
+    }
+
+    if (carry > 0) {
+        current.next = new ListNode(carry);
+    }
+
+    return dummyHead.next;
+}
