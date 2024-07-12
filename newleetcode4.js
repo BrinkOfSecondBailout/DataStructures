ac1e2c42381093fab163917341328aaa97786168
Danny Nguyen <nguyenduongpa@gmail.com>
2024-07-12 13:04:53 -0700

    d

--- newleetcode4.js
+++ newleetcode4.js
@@ -215,31 +215,113 @@ substring without repeating characters
 // It should exclude any single or double periods used to denote current or parent directories.
 // Return the new path.
 
-var simplifyPath = function(path) {
-    let parts = path.split('/');
-    let stack = [];
-    console.log(parts);
+// var simplifyPath = function(path) {
+//     let parts = path.split('/');
+//     let stack = [];
+//     console.log(parts);
 
-    for (let part of parts) {
-        if (part === '' || part === '.') {
-            continue;
-        } else if (part === '..') {
-            if (stack.length > 0) {
-                stack.pop();
-            }
-        } else {
-            stack.push(part);
-        }
-    }
+//     for (let part of parts) {
+//         if (part === '' || part === '.') {
+//             continue;
+//         } else if (part === '..') {
+//             if (stack.length > 0) {
+//                 stack.pop();
+//             }
+//         } else {
+//             stack.push(part);
+//         }
+//     }
 
-    let result = '/' + stack.join('/');
-    return result;
-}
+//     let result = '/' + stack.join('/');
+//     return result;
+// }
 
-console.log(simplifyPath("/home/"));
-console.log(simplifyPath("/home//foo/"));
-console.log(simplifyPath("/home/user/Documents/../Pictures"));
-console.log(simplifyPath("/../"));
-console.log(simplifyPath("/.../a/../b/c/../d/./"));
+// console.log(simplifyPath("/home/"));
+// console.log(simplifyPath("/home//foo/"));
+// console.log(simplifyPath("/home/user/Documents/../Pictures"));
+// console.log(simplifyPath("/../"));
+// console.log(simplifyPath("/.../a/../b/c/../d/./"));
 
 
+
+
+
+// Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+
+// Each row must contain the digits 1-9 without repetition.
+// Each column must contain the digits 1-9 without repetition.
+// Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+// Note:
+
+// A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+// Only the filled cells need to be validated according to the mentioned rules.
+
+// var isValidSudoku = function(board) {
+//     let rows = Array.from({ length: 9}, () => new Set());
+//     let columns = Array.from({ length: 9}, () => new Set());
+//     let boxes = Array.from({ length: 9}, () => new Set());
+
+//     for (let r = 0; r < 9; r++) {
+//         for (let c = 0; c < 9, c++) {
+//             let val = board[r][c];
+//             if (val === '.') continue;
+
+//             let boxIndex = Math.floor(r / 3) * 3 + Math.floor(c / 3);
+
+//             if (rows[r].has(val) || columns[c].has(val) || boxes[boxIndex].has(val)) {
+//                 return false;
+//             }
+
+//             rows[r].add(val);
+//             columns[c].add(val);
+//             boxes[boxIndex].add(val);
+//         }
+//     }
+
+//     return true;
+// };
+
+
+// var isValidSudoku = function(board) {
+//     let rows = createSetOfArrays(9), columns = createSetOfArrays(9), boxes = createSetOfArrays(9);
+
+//     for (let r = 0; r < 9; r++) {
+//         for (let c = 0; c < 9; c++) {
+//             let val = board[r][c];
+
+//             if (val === '.') continue;
+
+//             let boxIndex = Math.floor(r / 3) * 3 + Math.floor(c / 3);
+
+//             if (rows[r].has(val) || columns[c].has(val) || boxes[boxIndex].has(val)) {
+//                 return false;
+//             }
+//             rows[r].add(val);
+//             columns[c].add(val);
+//             boxes[boxIndex].add(val);
+//         }
+//     }
+
+//     return true;
+// }
+
+// function createSetOfArrays(length) {
+//     let array = []
+//     for (let i = 0; i < length; i++) {
+//         array.push(new Set());
+//     }
+//     return array;
+// }
+
+// let board = [
+//     ["5","3",".",".","7",".",".",".","."],
+//     ["6",".",".","1","9","5",".",".","."],
+//     [".","9","8",".",".",".",".","6","."],
+//     ["8",".",".",".","6",".",".",".","3"],
+//     ["4",".",".","8",".","3",".",".","1"],
+//     ["7",".",".",".","2",".",".",".","6"],
+//     [".","6",".",".",".",".","2","8","."],
+//     [".",".",".","4","1","9",".",".","5"],
+//     [".",".",".",".","8",".",".","7","9"]
+// ];
+// console.log(isValidSudoku(board));
