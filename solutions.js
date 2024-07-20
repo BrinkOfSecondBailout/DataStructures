f8021f91851c47d9fc7afc57417f164d21d418e0
Danny Nguyen <nguyenduongpa@gmail.com>
2024-07-19 20:09:50 -0700

    d

--- solutions.js
+++ solutions.js
@@ -1211,3 +1211,26 @@ substring without repeating characters
 // MinStack.prototype.getMin = function() {
 //     return this.minStack[this.minStack.length - 1];
 // }
+
+
+
+// var rotate = function(matrix) {
+//     const n = matrix.length;
+
+// Transpose
+//     for (let i = 0; i < n; i++) {
+//         for (let j = i + 1; j < n; j++) {
+//             [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
+//         }
+//     }
+
+// Reverse
+//     for (let i = 0; i < n; i++) {
+//         matrix[i].reverse();
+//     }
+// };
+
+// let matrix = [[1,2,3],[4,5,6],[7,8,9]];
+// rotate(matrix);
+// console.log(matrix);
+
