9798039a9a1a025425e2bad92716ec3f3a2b2c0e
Danny Nguyen <nguyenduongpa@gmail.com>
2024-07-21 17:43:58 -0700

    d

--- solutions.js
+++ solutions.js
@@ -1505,3 +1505,34 @@ substring without repeating characters
 
 
 
+
+// var productExceptSelf = function(nums) {
+//     let n = nums.length;
+
+//     let result = new Array(n);
+//     let left = new Array(n).fill(1);
+//     let right = new Array(n).fill(1);
+
+//     for (let i = 1; i < n; i++) {
+//         left[i] = left[i - 1] * nums[i - 1];
+//     }
+
+//     for (let i = n - 2; i >= 0; i--) {
+//         right[i] = right[i + 1] * nums[i + 1];
+//     }
+
+//     for (let i = 0; i < n; i++) {
+//         result[i] = left[i] * right[i];
+//     }
+
+//     return result;
+// };
+
+
+// nums = [1,2,3,4];
+// console.log(productExceptSelf(nums));
+
+
+
+
+
